ecba2b4219ced44691b7ec2d93460ddde0e7273f
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-19 16:34:01 -0300

    Salvar usuario na planilha iniciado

--- ProjetoSA/LeitorPlanilha.cs
+++ ProjetoSA/LeitorPlanilha.cs
@@ -5,6 +5,8 @@ using System.Windows.Forms;
 public class LeitorPlanilha {
 
     public static string CaminhoPlanilhaLogin = @"..\..\..\Recursos\BancoDeDados.xlsx";
-    
 
+    public static string AdicionarUsuario(string nome, string senha) {
+        
+    }
 }
