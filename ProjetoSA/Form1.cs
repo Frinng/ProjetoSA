c054dccf9dc8e4f2f44f9b3dec36cab2f3d9ad58
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-19 13:10:15 -0300

    Adicionei labels

--- ProjetoSA/Form1.cs
+++ ProjetoSA/Form1.cs
@@ -25,8 +25,40 @@ public partial class Formprinciapl : Form {
         this.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoDeTela.png");
         this.BackgroundImageLayout = ImageLayout.Stretch;
         
+        //INICIO DO CODIGO
         
+        //LAbel falando Bem Vindo
+        Label TextoOlaAmigo = new Label();
+        TextoOlaAmigo.Text = "Olá, Amigo!";
+        TextoOlaAmigo.Left = 650; //Define a posicao horizontal(vulgo X) do label dentro do forms
+        TextoOlaAmigo.Top = 260; //Define a posicao vertical(vulgo Y) do label dentro do forms
+        TextoOlaAmigo.AutoSize = true;
+        TextoOlaAmigo.Font = new Font("Arial",24, FontStyle.Bold);
+        TextoOlaAmigo.ForeColor = Color.White;
+        TextoOlaAmigo.BackColor = Color.Transparent; //Deixa o fundo transparente
+        this.Controls.Add(TextoOlaAmigo); // obviamente adiciona o label
+
+        //Label do insira detalhes
+        Label InsiraDetalhes = new Label();
+        InsiraDetalhes.Text = "Insira alguns Detalhes e comece sua jornada";
+        InsiraDetalhes.Left = 600;
+        InsiraDetalhes.Top = 300;
+        InsiraDetalhes.AutoSize = true;
+        InsiraDetalhes.Font = new Font("Arial", 10, FontStyle.Bold);
+        InsiraDetalhes.ForeColor = Color.White;
+        InsiraDetalhes.BackColor = Color.Transparent;
+        this.Controls.Add(InsiraDetalhes);
         
+        //COntinuacao do insira detalhes
+        Label Conosco = new Label();
+        Conosco.Text = "conosco.";
+        Conosco.Left = 720;
+        Conosco.Top = 315;
+        Conosco.AutoSize = true;
+        Conosco.Font = new Font("Arial", 10, FontStyle.Bold);
+        Conosco.ForeColor = Color.White;
+        Conosco.BackColor = Color.Transparent;
+        this.Controls.Add(Conosco);
 
     }
 }
