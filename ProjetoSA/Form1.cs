a56b5ab9a178eac3a19ea53b92643b2354cb274f
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-19 15:19:22 -0300

    Tela Login Compleata

--- ProjetoSA/Form1.cs
+++ ProjetoSA/Form1.cs
@@ -33,8 +33,7 @@ public partial class Formprinciapl : Form {
             //LAbel falando Bem Vindo
         Label TextoOlaAmigo = new Label();
         TextoOlaAmigo.Text = "Olá, Amigo!";
-        TextoOlaAmigo.Left = 650; //Define a posicao horizontal(vulgo X) do label dentro do forms
-        TextoOlaAmigo.Top = 260; //Define a posicao vertical(vulgo Y) do label dentro do forms
+        TextoOlaAmigo.Location = new Point(650, 200);//Define a posicao horizontal(vulgo X) do label dentro do forms e a posicao vertical(vulgo Y) do label dentro do forms
         TextoOlaAmigo.AutoSize = true;
         TextoOlaAmigo.Font = new Font("Arial",24, FontStyle.Bold);
         TextoOlaAmigo.ForeColor = Color.White;
@@ -44,8 +43,7 @@ public partial class Formprinciapl : Form {
             //Label do insira detalhes
         Label InsiraDetalhes = new Label();
         InsiraDetalhes.Text = "Insira alguns Detalhes e comece sua jornada";
-        InsiraDetalhes.Left = 590;
-        InsiraDetalhes.Top = 300;
+        InsiraDetalhes.Location = new Point(590,240);
         InsiraDetalhes.AutoSize = true;
         InsiraDetalhes.Font = new Font("Arial", 10, FontStyle.Bold);
         InsiraDetalhes.ForeColor = Color.White;
@@ -55,8 +53,7 @@ public partial class Formprinciapl : Form {
             //COntinuacao do insira detalhes
         Label Conosco = new Label();
         Conosco.Text = "conosco.";
-        Conosco.Left = 710;
-        Conosco.Top = 315;
+        Conosco.Location = new Point(710, 260);
         Conosco.AutoSize = true;
         Conosco.Font = new Font("Arial", 10, FontStyle.Bold);
         Conosco.ForeColor = Color.White;
@@ -67,13 +64,11 @@ public partial class Formprinciapl : Form {
         Button BotaoRegistrar = new Button();
         BotaoRegistrar.Text = "REGISTRAR";
         BotaoRegistrar.AutoSize = true;
-        BotaoRegistrar.Left = 650;
-        BotaoRegistrar.Top = 350;
+        BotaoRegistrar.Location = new Point(650, 290);
         BotaoRegistrar.Font = new Font("Arial",20,FontStyle.Bold);
         BotaoRegistrar.ForeColor = Color.White;
         BotaoRegistrar.BackColor = Color.FromArgb(0, 171, 155);
         BotaoRegistrar.FlatStyle = FlatStyle.Flat; //tira a borda azul quando o mouse passa por cima
-        BotaoRegistrar.FlatAppearance.BorderSize = 0; //tira a bora azul quando o mouse passa por cima
         BotaoRegistrar.FlatAppearance.BorderColor = Color.White;
         BotaoRegistrar.FlatAppearance.BorderSize = 1;
         BotaoRegistrar.Click += (sender, e) => { }; //quando clicam no botao o ponteiro manda para a funcao
@@ -90,7 +85,7 @@ public partial class Formprinciapl : Form {
         Entrar_NoSistema.BackColor = Color.Transparent;
         this.Controls.Add(Entrar_NoSistema);
         
-        //Campo de texto
+            //Campo de texto
         TextBox CampoUsuario = new TextBox();
         CampoUsuario.Name = "campousuario";
         CampoUsuario.Location = new Point(120, 280);
@@ -100,7 +95,7 @@ public partial class Formprinciapl : Form {
         CampoUsuario.TextAlign = HorizontalAlignment.Center;
         this.Controls.Add(CampoUsuario);
         
-        //campodetexto senha
+            //campodetexto senha
         TextBox CampoSenha = new TextBox();
         CampoSenha.Name = "camposenha";
         CampoSenha.Location = new Point(120, 330);
@@ -109,5 +104,32 @@ public partial class Formprinciapl : Form {
         CampoSenha.PlaceholderText = "Senha";
         CampoSenha.TextAlign = HorizontalAlignment.Center;
         this.Controls.Add(CampoSenha);
+        
+            //Botao do esqueceu sua senha
+        Button BotaoEsqueceuSenha = new Button();
+        BotaoEsqueceuSenha.Text = "Esqueceu sua senha?";
+        BotaoEsqueceuSenha.Location = new Point(160, 380);
+        BotaoEsqueceuSenha.AutoSize = true;
+        BotaoEsqueceuSenha.Font = new Font("Arial", 10, FontStyle.Bold);
+        BotaoEsqueceuSenha.ForeColor = Color.FromArgb(0, 171, 155);
+        BotaoEsqueceuSenha.BackColor = Color.White;
+        BotaoEsqueceuSenha.FlatStyle = FlatStyle.Flat;
+        BotaoEsqueceuSenha.FlatAppearance.BorderColor = Color.White;
+        BotaoEsqueceuSenha.Click += (sender, e) => { };
+        this.Controls.Add(BotaoEsqueceuSenha);
+
+        //Botao entrar
+        Button BotaoEntrar = new Button();
+        BotaoEntrar.Text = "ENTRAR";
+        BotaoEntrar.AutoSize = true;
+        BotaoEntrar.Location = new Point(175, 410);
+        BotaoEntrar.Font = new Font("Arial", 20, FontStyle.Bold);
+        BotaoEntrar.ForeColor = Color.FromArgb(0, 171, 155);
+        BotaoEntrar.FlatStyle = FlatStyle.Flat;
+        BotaoEntrar.FlatAppearance.BorderColor = Color.FromArgb(0, 171, 155);
+        BotaoEntrar.BackColor = Color.White;
+        BotaoEntrar.FlatAppearance.BorderSize = 1;
+        BotaoEntrar.Click += (sender, e) => { };
+        this.Controls.Add(BotaoEntrar);
     }
 }
