834245e2e40838911704d845ca02a70ab13eefae
Pedro H <167120714+Frinng@users.noreply.github.com>
2026-04-21 20:02:04 -0300

    Divisão completa

--- ProjetoSA/CriarMenuCliente.cs
+++ ProjetoSA/CriarMenuCliente.cs
@@ -2,6 +2,221 @@
 
 public partial class Formprinciapl {
     
+    public void MenuCliente() {
+		menucliente = new Panel();
+		menucliente.Name = "Temporario";
+		menucliente.Size = this.ClientSize;
+		menucliente.Location = new Point(0, 0);
+		menucliente.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+		menucliente.BackgroundImageLayout = ImageLayout.Stretch;
+		menucliente.Visible = false;
+
+
+		Label labelMenuCliente = new Label();
+		labelMenuCliente.Text = "Menu Cliente";
+		labelMenuCliente.Location = new Point(470, 100);
+		labelMenuCliente.Font = new Font("Arial", 28, FontStyle.Bold);
+		labelMenuCliente.AutoSize = true;
+		labelMenuCliente.BackColor = Color.Transparent;
+		labelMenuCliente.ForeColor = Color.FromArgb(255, 189, 89);
+		menucliente.Controls.Add(labelMenuCliente);
+
+
+		Button BotaoCaracteristicas = new Button();
+		BotaoCaracteristicas.Text = "Caracteristicas";
+		BotaoCaracteristicas.Size = new Size(230, 40);
+		BotaoCaracteristicas.Location = new Point(480, 180);
+		BotaoCaracteristicas.Font = new Font("Arial", 20, FontStyle.Bold);
+		BotaoCaracteristicas.ForeColor = Color.FromArgb(255, 189, 89);
+		BotaoCaracteristicas.FlatStyle = FlatStyle.Flat;
+		BotaoCaracteristicas.FlatAppearance.BorderColor = Color.FromArgb(255, 189, 89);
+		BotaoCaracteristicas.BackColor = Color.White;
+		BotaoCaracteristicas.FlatAppearance.BorderSize = 1;
+		BotaoCaracteristicas.Click += (sender, e) => {
+
+			menucliente.Visible = false;
+			Menucaract.Visible = true;
+
+		};
+		menucliente.Controls.Add(BotaoCaracteristicas);
+
+
+		Button BotaoInfoLotes = new Button();
+		BotaoInfoLotes.Text = "Info Lotes";
+		BotaoInfoLotes.Size = new Size(230, 40);
+		BotaoInfoLotes.Location = new Point(480, 230);
+		BotaoInfoLotes.Font = new Font("Arial", 20, FontStyle.Bold);
+		BotaoInfoLotes.ForeColor = Color.FromArgb(255, 189, 89);
+		BotaoInfoLotes.FlatStyle = FlatStyle.Flat;
+		BotaoInfoLotes.FlatAppearance.BorderColor = Color.FromArgb(255, 189, 89);
+		BotaoInfoLotes.BackColor = Color.White;
+		BotaoInfoLotes.FlatAppearance.BorderSize = 1;
+		BotaoInfoLotes.Click += (sender, e) => {
+			menucliente.Visible = false;
+			Menuinfolotes.Visible = true;
+		};
+		menucliente.Controls.Add(BotaoInfoLotes);
+
+		Button BotaoCarrinho = new Button();
+		BotaoCarrinho.Text = "Carrinho";
+		BotaoCarrinho.Size = new Size(230, 40);
+		BotaoCarrinho.Location = new Point(480, 280);
+		BotaoCarrinho.Font = new Font("Arial", 20, FontStyle.Bold);
+		BotaoCarrinho.ForeColor = Color.FromArgb(255, 189, 89);
+		BotaoCarrinho.FlatStyle = FlatStyle.Flat;
+		BotaoCarrinho.FlatAppearance.BorderColor = Color.FromArgb(255, 189, 89);
+		BotaoCarrinho.BackColor = Color.White;
+		BotaoCarrinho.FlatAppearance.BorderSize = 1;
+		BotaoCarrinho.Click += (sender, e) => {
+			menucliente.Visible = false;
+			Menucarrinho.Visible = true;
+		};
+		menucliente.Controls.Add(BotaoCarrinho);
+
+		Button BotaoRastreio = new Button();
+		BotaoRastreio.Text = "Rastreio";
+		BotaoRastreio.Size = new Size(230, 40);
+		BotaoRastreio.Location = new Point(480, 330);
+		BotaoRastreio.Font = new Font("Arial", 20, FontStyle.Bold);
+		BotaoRastreio.ForeColor = Color.FromArgb(255, 189, 89);
+		BotaoRastreio.FlatStyle = FlatStyle.Flat;
+		BotaoRastreio.FlatAppearance.BorderColor = Color.FromArgb(255, 189, 89);
+		BotaoRastreio.BackColor = Color.White;
+		BotaoRastreio.FlatAppearance.BorderSize = 1;
+		BotaoRastreio.Click += (sender, e) => {
+			menucliente.Visible = false;
+			Menurastreio.Visible = true;
+		};
+		menucliente.Controls.Add(BotaoRastreio);
+
+		//Botao para voltar
+		Button botaoVoltar = new Button();
+		botaoVoltar.Text = "Voltar";
+		botaoVoltar.Location = new Point(20, 20);
+		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+		botaoVoltar.AutoSize = true;
+		botaoVoltar.FlatStyle = FlatStyle.Flat;
+		botaoVoltar.FlatAppearance.BorderSize = 0;
+		botaoVoltar.BackColor = Color.White;
+		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+		botaoVoltar.Click += (sender, e) => {
+			menucliente.Visible = false;
+			MENUFORNECEDOR.Visible = true;
+		};
+		menucliente.Controls.Add(botaoVoltar);
+	}
+    
+    public void MenuCaracteristicas() {
+		Menucaract = new Panel();
+		Menucaract.Name = "Temporario";
+		Menucaract.Size = this.ClientSize;
+		Menucaract.Location = new Point(0, 0);
+		Menucaract.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+		Menucaract.BackgroundImageLayout = ImageLayout.Stretch;
+		Menucaract.Visible = false;
+		
+		
+		
+		Button botaoVoltar = new Button();
+		botaoVoltar.Text = "Voltar";
+		botaoVoltar.Location = new Point(20, 20);
+		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+		botaoVoltar.AutoSize = true;
+		botaoVoltar.FlatStyle = FlatStyle.Flat;
+		botaoVoltar.FlatAppearance.BorderSize = 0;
+		botaoVoltar.BackColor = Color.White;
+		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+		botaoVoltar.Click += (sender, e) => {
+			Menucaract.Visible = false;
+			menucliente.Visible = true;
+		};
+		Menucaract.Controls.Add(botaoVoltar);
+		
+	}
+
+    public void MenuInfoLotes() {
+		Menuinfolotes = new Panel();
+		Menuinfolotes.Name = "Temporario";
+		Menuinfolotes.Size = this.ClientSize;
+		Menuinfolotes.Location = new Point(0, 0);
+		Menuinfolotes.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+		Menuinfolotes.BackgroundImageLayout = ImageLayout.Stretch;
+		Menuinfolotes.Visible = false;
+		
+		
+		
+		
+		Button botaoVoltar = new Button();
+		botaoVoltar.Text = "Voltar";
+		botaoVoltar.Location = new Point(20, 20);
+		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+		botaoVoltar.AutoSize = true;
+		botaoVoltar.FlatStyle = FlatStyle.Flat;
+		botaoVoltar.FlatAppearance.BorderSize = 0;
+		botaoVoltar.BackColor = Color.White;
+		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+		botaoVoltar.Click += (sender, e) => {
+			Menuinfolotes.Visible = false;
+			menucliente.Visible = true;
+		};
+		Menuinfolotes.Controls.Add(botaoVoltar);
+	}
+	
+    public void MenuCarrinho() {
+		Menucarrinho = new Panel();
+		Menucarrinho.Name = "Temporario";
+		Menucarrinho.Size = this.ClientSize;
+		Menucarrinho.Location = new Point(0, 0);
+		Menucarrinho.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+		Menucarrinho.BackgroundImageLayout = ImageLayout.Stretch;
+		Menucarrinho.Visible = false;
+		
+		
+		
+		Button botaoVoltar = new Button();
+		botaoVoltar.Text = "Voltar";
+		botaoVoltar.Location = new Point(20, 20);
+		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+		botaoVoltar.AutoSize = true;
+		botaoVoltar.FlatStyle = FlatStyle.Flat;
+		botaoVoltar.FlatAppearance.BorderSize = 0;
+		botaoVoltar.BackColor = Color.White;
+		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+		botaoVoltar.Click += (sender, e) => {
+			Menucarrinho.Visible = false;
+			menucliente.Visible = true;
+		};
+		Menucarrinho.Controls.Add(botaoVoltar);
+	}
+
+    public void MenuRastreio() {
+		Menurastreio = new Panel();
+		Menurastreio.Name = "Temporario";
+		Menurastreio.Size = this.ClientSize;
+		Menurastreio.Location = new Point(0, 0);
+		Menurastreio.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+		Menurastreio.BackgroundImageLayout = ImageLayout.Stretch;
+		Menurastreio.Visible = false;
+		
+		
+		
+		Button botaoVoltar = new Button();
+		botaoVoltar.Text = "Voltar";
+		botaoVoltar.Location = new Point(20, 20);
+		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+		botaoVoltar.AutoSize = true;
+		botaoVoltar.FlatStyle = FlatStyle.Flat;
+		botaoVoltar.FlatAppearance.BorderSize = 0;
+		botaoVoltar.BackColor = Color.White;
+		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+		botaoVoltar.Click += (sender, e) => {
+			Menurastreio.Visible = false;
+			menucliente.Visible = true;
+		};
+		Menurastreio.Controls.Add(botaoVoltar);
+		
+	}
+    
     
     
     
--- ProjetoSA/CriarMenuFornecedor.cs
+++ ProjetoSA/CriarMenuFornecedor.cs
@@ -1,5 +1,13 @@
 ﻿namespace ProjetoSA;
 
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Net.Http;
+using System.Reflection.PortableExecutable;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 public partial class Formprinciapl {
     
     public void MenuFornecedor() {
@@ -218,4 +226,150 @@ public partial class Formprinciapl {
 		Menucontrato.Controls.Add(botaoVoltar);
 	}
 	
+    public async void MenuVerContratos() {
+    		Menuvercontratos = new Panel();
+    		Menuvercontratos.Name = "Temporario";
+    		Menuvercontratos.Size = this.ClientSize;
+    		Menuvercontratos.Location = new Point(0, 0);
+    		Menuvercontratos.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+    		Menuvercontratos.BackgroundImageLayout = ImageLayout.Stretch;
+    		Menuvercontratos.Visible = false;
+    		
+    		Label labelAPE = new Label();
+    		labelAPE.Text = "Contratos";
+    		labelAPE.Location = new Point(80, 100);
+    		labelAPE.Font = new Font("Arial", 28, FontStyle.Bold);
+    		labelAPE.AutoSize = true;
+    		labelAPE.BackColor = Color.Transparent;
+    		labelAPE.ForeColor = Color.FromArgb(255, 189, 89);
+    		Menuvercontratos.Controls.Add(labelAPE);
+    		
+    		FlowLayoutPanel flowVerContratos = new FlowLayoutPanel();
+    		flowVerContratos.Location = new Point(50, 150);
+    		flowVerContratos.Size = new Size(1100, 400);
+    		flowVerContratos.BackColor = Color.Transparent;
+    		flowVerContratos.AutoScroll = true;
+    		flowVerContratos.FlowDirection = FlowDirection.LeftToRight;
+    		flowVerContratos.WrapContents = true;
+    		flowVerContratos.Padding = new Padding(20, 20, 20, 50);
+    		Menuvercontratos.Controls.Add(flowVerContratos);
+    		try {
+    			HttpClient client = new HttpClient();
+    	        // Lembre-se de criar esse PHP para retornar os dados da sua tabela 'contrato'
+    	        string url = "http://localhost/projeto_sa/listar_contratos.php"; 
+    	        string response = await client.GetStringAsync(url);
+    	        
+    	        // Aqui usamos a classe que representa sua tabela SQL 'contrato'
+    	        var listaContratos = JsonConvert.DeserializeObject<List<Animal.ContratoDTO>>(response);
+    
+    	        if (listaContratos != null) {
+    	            foreach (var contrato in listaContratos) {
+    	                // Criando o Card (Painel)
+    	                Panel card = new Panel();
+    	                card.Size = new Size(300, 220);
+    	                card.BackColor = Color.White; // Card branco com borda amarela fica elegante
+    	                card.BorderStyle = BorderStyle.FixedSingle;
+    	                card.Margin = new Padding(15);
+    
+    	                // Cabeçalho do Card (ID do Contrato)
+    	                Label lblId = new Label();
+    	                lblId.Text = "Contrato #" + contrato.id_contrato;
+    	                lblId.Font = new Font("Arial", 14, FontStyle.Bold);
+    	                lblId.ForeColor = Color.FromArgb(255, 189, 89);
+    	                lblId.Size = new Size(300, 40);
+    	                lblId.TextAlign = ContentAlignment.MiddleCenter;
+    	                lblId.Location = new Point(0, 10);
+    
+    	                // Detalhes do Contrato
+    	                Label lblCorpo = new Label();
+    	                // Formatando Moeda e Data
+    	                lblCorpo.Text = $"Valor: R$ {contrato.valor_total:N2}\n" +
+    	                               $"Vence em: {contrato.data_vencimento:dd/MM/yyyy}\n" +
+    	                               $"Status: {contrato.status_contrato}";
+    	                lblCorpo.Font = new Font("Arial", 11, FontStyle.Regular);
+    	                lblCorpo.Size = new Size(300, 80);
+    	                lblCorpo.Location = new Point(0, 60);
+    	                lblCorpo.TextAlign = ContentAlignment.MiddleCenter;
+    
+    	                // Botão "Ver Termos" dentro do Card
+    	                Button btnVerTermos = new Button();
+    	                btnVerTermos.Text = "VER CLAÚSULAS";
+    	                btnVerTermos.Size = new Size(150, 30);
+    	                btnVerTermos.Location = new Point(75, 130);
+    	                btnVerTermos.FlatStyle = FlatStyle.Flat;
+    	                btnVerTermos.BackColor = Color.FromArgb(255, 189, 89);
+    	                btnVerTermos.ForeColor = Color.White;
+    	                btnVerTermos.Click += (s, e) => {
+    	                    MessageBox.Show("Termos do Contrato:\n\n" + contrato.termos_clausulas);
+    	                };
+    	                
+    	                Button btnPdf = new Button();
+    	                btnPdf.Text = "EXPORTAR PDF";
+    	                btnPdf.Size = new Size(150, 20);
+    	                btnPdf.Location = new Point(75, 165); // Abaixo do botão de ver termos
+    	                btnPdf.FlatStyle = FlatStyle.Flat;
+    	                btnPdf.BackColor = Color.FromArgb(255, 189, 89);
+    	                btnPdf.ForeColor = Color.White;
+    
+    	                btnPdf.Click += (s, e) => {
+    		                GeraPDF.GerarPdfContratoFormatoJuridico(contrato);
+    	                };
+    
+    	                card.Controls.Add(btnPdf);
+    	                card.Controls.Add(lblId);
+    	                card.Controls.Add(lblCorpo);
+    	                card.Controls.Add(btnVerTermos);
+    
+    	                flowVerContratos.Controls.Add(card);
+    	            }
+    	        }
+    		}
+    		catch (Exception ex) {
+    			MessageBox.Show("Erro ao carregar contrato: " + ex.Message);
+    		}
+    		
+    		Button botaoVoltar = new Button();
+    		botaoVoltar.Text = "Voltar";
+    		botaoVoltar.Location = new Point(20, 20);
+    		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+    		botaoVoltar.AutoSize = true;
+    		botaoVoltar.FlatStyle = FlatStyle.Flat;
+    		botaoVoltar.FlatAppearance.BorderSize = 0;
+    		botaoVoltar.BackColor = Color.White;
+    		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+    		botaoVoltar.Click += (sender, e) => {
+    			Menuvercontratos.Visible = false;
+    			Menucontrato.Visible = true;
+    		};
+    		Menuvercontratos.Controls.Add(botaoVoltar);
+    	}
+    
+	public void MenuNovosContratos() {
+    		Menunovoscontratos = new Panel();
+    		Menunovoscontratos.Name = "Temporario";
+    		Menunovoscontratos.Size = this.ClientSize;
+    		Menunovoscontratos.Location = new Point(0, 0);
+    		Menunovoscontratos.BackgroundImage = Image.FromFile(@"..\..\..\Recursos\FundoTelaPadrao.png");
+    		Menunovoscontratos.BackgroundImageLayout = ImageLayout.Stretch;
+    		Menunovoscontratos.Visible = false;
+    		
+    		
+    		
+    		Button botaoVoltar = new Button();
+    		botaoVoltar.Text = "Voltar";
+    		botaoVoltar.Location = new Point(20, 20);
+    		botaoVoltar.Font = new Font("Arial", 12, FontStyle.Bold);
+    		botaoVoltar.AutoSize = true;
+    		botaoVoltar.FlatStyle = FlatStyle.Flat;
+    		botaoVoltar.FlatAppearance.BorderSize = 0;
+    		botaoVoltar.BackColor = Color.White;
+    		botaoVoltar.ForeColor = Color.FromArgb(255, 189, 89);
+    		botaoVoltar.Click += (sender, e) => {
+    			Menunovoscontratos.Visible = false;
+    			Menucontrato.Visible = true;
+    		};
+    		Menunovoscontratos.Controls.Add(botaoVoltar);
+    	}
+    
+    
 }
